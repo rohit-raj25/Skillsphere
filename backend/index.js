6ad33300ffbe41c2a566bcb3d623846d887beb99
rohit-raj25 <rohitraj250310@gmail.com>
2023-11-18 07:01:01 +0530

    Adding frontend with backend

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose, { MongooseError } from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
+
+//Middleware for handling CORS POLICY
+//opt1:Allow all origins with default of cors(*)
+app.use(cors());
+//opt2:Allow Cuztom origins
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["content-Type"],
+//   })
+// );
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("hello mern");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("app connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
